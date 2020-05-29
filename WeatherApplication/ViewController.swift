a87e89f0f32f4266608ac9ffdd9ae89ec18c7b3c
Andrii Shevchuk <and.sheva2017@gmail.com>
2020-05-29 13:34:45 +0300

    Initial Commit

---
+++ WeatherApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApplication
+//
+//  Created by Андрей Шевчук on 29.05.2020.
+//  Copyright © 2020 Андрей Шевчук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
